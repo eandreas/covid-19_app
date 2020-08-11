951a8c74746a2ec3f3b19faa8d38b5018892247a
Andreas Dorta <eichi@gmx.ch>
2020-08-11 12:41:34 +0200

    Update color of bars only, not whole graph

--- app.py
+++ app.py
@@ -80,12 +80,7 @@ def update_figure(date_range_slider):
         else:
             cols.append('lightsalmon')
 
-    fig_new_conf = px.bar(df, x="date", y=["new_conf"], barmode="group")
-    fig_new_conf.update_layout(showlegend=False)
-    fig_new_conf.update_layout(plot_bgcolor=colors['background'], paper_bgcolor=colors['background'], font_color=colors['text'])
-    fig_new_conf.update_traces(marker_color=cols, marker_line_color='rgb(8,48,107)',
-                  marker_line_width=0, opacity=0.6)
-    fig_new_conf.update_layout(title_text='Daily confirmed cases - Switzerland')   
+    fig_new_conf.update_traces(marker_color=cols)
     return fig_new_conf
 
 # update new_conf_zoomed figure
@@ -110,6 +105,10 @@ def update_zoomed_figure(date_range_slider):
                   marker_line_width=0, opacity=0.6)
     fig_new_conf_zoomed.update_layout(title_text='Daily confirmed cases (zoomed) - Switzerland')
     return fig_new_conf_zoomed
+
+app.css.append_css({
+    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
+})
 
 if __name__ == '__main__':
     # True for hot reloading
